fc065841e32725eb4fc08ee1e60db34a7cabb0a4
ga15 <manas.dash@tesco.com>
2020-02-22 17:34:22 +0530

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+# Write a Python program which iterates the integers from 1 to 50.
+# For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+# For numbers which are multiples of both three and five print "FizzBuzz".
+
+for fizzbuzz in range(51):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
+
+
+
