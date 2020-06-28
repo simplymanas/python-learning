e09047af257e87a850221ce41ee841f07843bc1d
Manas Dash <manas.dash@tesco.com>
2020-06-28 22:51:37 +0530

    adding palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+
+# verify if a string is palindrome or not
+# avoid using built in function
+
+input_string = input ("Enter the string : ")
+
+if input_string[::-1].lower() == input_string.lower():
+	print ("Oh My God!!! that's a palindrome 👏")
+else:
+	print ("Try again ")
