f27aa460ce1bf8371a4551147e39552ddade6bb8
Manas Dash <manas.dash@tesco.com>
2020-07-10 22:03:14 +0530

    adding DictionaryByExample.py

---
+++ DictionaryByExample.py
@@ -0,0 +1,24 @@
+
+# 10th July 2020
+# Manas Dash
+# Dictionary Keys
+# almost anything as key in python in a dictionary
+
+kuchbhi = {
+	'army': 'the best',
+	'pee-kay': 'gir pade',
+	1: '2 ka 4',
+	True: 'Earth is a planet',
+	False: 'Mango is a vegetable',
+	('True', 'False'): 'Boolean Type'
+}
+
+print (kuchbhi['army'])
+print (kuchbhi['pee-kay'])
+print (kuchbhi[('True', 'False')])
+print (kuchbhi[1])
+print (kuchbhi[False])
+print (kuchbhi[True])
+
+#  and the cheating here is 1 and True ,
+#  test them again (replace their sequence in the dict)
